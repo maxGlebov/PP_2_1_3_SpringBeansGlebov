e2fcb485cca793a55f71c4c7d59960d1d610c284
Maxim Glebov <maglebov@gmail.com>
2024-04-04 14:53:15 +0300

    Initial commit

---
+++ src/main/java/app/model/Dog.java
@@ -0,0 +1,25 @@
+package app.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dog extends Animal{
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
